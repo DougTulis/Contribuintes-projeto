e0bfd5df9c11c5f0c753f22580988ea40e4f8696
Douglas Engratulis <dengratulis@gmail.com>
2024-10-31 18:37:03 -0300

    Criado construtor, atributos e getters e setters

--- TaxPayer.cs
+++ TaxPayer.cs
@@ -6,5 +6,19 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp2 {
     internal class TaxPayer {
+        public double salaryIncome { get; set; }
+        public double servicesIncome { get; set; }
+        public double capitalIncome { get; set; }
+        public double healthSpending { get; set; }
+        public double educationSpending { get; set; }
+
+        public TaxPayer(double salaryIncome, double servicesIncome, double capitalIncome, double healthSpending, double educationSpending) {
+            this.salaryIncome = salaryIncome;
+            this.servicesIncome = servicesIncome;
+            this.capitalIncome = capitalIncome;
+            this.healthSpending = healthSpending;
+            this.educationSpending = educationSpending;
+        }
+
     }
 }
